0a9b3a6fc5110db377b07ecde9877cbceae0da15
Mohamed Hassan <mohamed.hassn.dev@gmail.com>
2021-10-22 06:24:14 +0200

    use react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React, { useState, useEffect, useRef } from "react";
+import { Switch, Route, Link } from "react-router-dom";
 import * as BooksAPI from "./BooksAPI";
 import "./App.css";
 import Book from "./components/Book";
 import { BookItem, Shelf } from "./interfaces/interfaces";
 
 function BooksApp() {
-  const [showSearchPage, setShowSearchPage] = useState(false);
   const [books, setBooks] = useState<BookItem[]>([]);
   const [searchResult, setSearchResult] = useState<BookItem[]>([]);
   const [noResult, setNoResult] = useState(false);
@@ -44,7 +44,6 @@ function BooksApp() {
     const signal = abortController.current.signal;
     BooksAPI.search(query, signal)
       .then((books) => {
-        console.log("books", books);
         if (books && !books.error) {
           setSearchResult(books);
         } else {
@@ -59,17 +58,79 @@ function BooksApp() {
 
   return (
     <div className="app">
-      {showSearchPage ? (
-        <div className="search-books">
-          <div className="search-books-bar">
-            <button
-              className="close-search"
-              onClick={() => setShowSearchPage(false)}
-            >
-              Close
-            </button>
-            <div className="search-books-input-wrapper">
-              {/*
+      <Switch>
+        <Route exact path="/">
+          <div className="list-books">
+            <div className="list-books-title">
+              <h1>MyReads</h1>
+            </div>
+            <div className="list-books-content">
+              <div>
+                <div className="bookshelf">
+                  <h2 className="bookshelf-title">Currently Reading</h2>
+                  <div className="bookshelf-books">
+                    <ol className="books-grid">
+                      {books
+                        .filter((book) => book.shelf === "currentlyReading")
+                        .map((book) => (
+                          <Book
+                            key={book.id}
+                            book={book}
+                            setBookShelf={setBookShelf}
+                          />
+                        ))}
+                    </ol>
+                  </div>
+                </div>
+                <div className="bookshelf">
+                  <h2 className="bookshelf-title">Want to Read</h2>
+                  <div className="bookshelf-books">
+                    <ol className="books-grid">
+                      {books
+                        .filter((book) => book.shelf === "wantToRead")
+                        .map((book) => (
+                          <Book
+                            key={book.id}
+                            book={book}
+                            setBookShelf={setBookShelf}
+                          />
+                        ))}
+                    </ol>
+                  </div>
+                </div>
+                <div className="bookshelf">
+                  <h2 className="bookshelf-title">Read</h2>
+                  <div className="bookshelf-books">
+                    <ol className="books-grid">
+                      {books
+                        .filter((book) => book.shelf === "read")
+                        .map((book) => (
+                          <Book
+                            key={book.id}
+                            book={book}
+                            setBookShelf={setBookShelf}
+                          />
+                        ))}
+                    </ol>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="open-search">
+              <Link to="/search">
+                <button>Add a book</button>
+              </Link>
+            </div>
+          </div>
+        </Route>
+        <Route path="/search">
+          <div className="search-books">
+            <div className="search-books-bar">
+              <Link to="/">
+                <button className="close-search">Close</button>
+              </Link>
+              <div className="search-books-input-wrapper">
+                {/*
                   NOTES: The search from BooksAPI is limited to a particular set of search terms.
                   You can find these search terms here:
                   https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
@@ -77,87 +138,27 @@ function BooksApp() {
                   However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
                   you don't find a specific author or title. Every search is limited by search terms.
                 */}
-              <input
-                type="text"
-                onChange={(e) => {
-                  setNoResult(false);
-                  searchBooks(e.target.value);
-                }}
-                placeholder="Search by title or author"
-              />
-            </div>
-          </div>
-          <div className="search-books-results">
-            <ol className="books-grid">
-              {searchResult.map((book) => (
-                <Book key={book.id} book={book} setBookShelf={setBookShelf} />
-              ))}
-              {noResult && <div>No Results where found </div>}
-            </ol>
-          </div>
-        </div>
-      ) : (
-        <div className="list-books">
-          <div className="list-books-title">
-            <h1>MyReads</h1>
-          </div>
-          <div className="list-books-content">
-            <div>
-              <div className="bookshelf">
-                <h2 className="bookshelf-title">Currently Reading</h2>
-                <div className="bookshelf-books">
-                  <ol className="books-grid">
-                    {books
-                      .filter((book) => book.shelf === "currentlyReading")
-                      .map((book) => (
-                        <Book
-                          key={book.id}
-                          book={book}
-                          setBookShelf={setBookShelf}
-                        />
-                      ))}
-                  </ol>
-                </div>
-              </div>
-              <div className="bookshelf">
-                <h2 className="bookshelf-title">Want to Read</h2>
-                <div className="bookshelf-books">
-                  <ol className="books-grid">
-                    {books
-                      .filter((book) => book.shelf === "wantToRead")
-                      .map((book) => (
-                        <Book
-                          key={book.id}
-                          book={book}
-                          setBookShelf={setBookShelf}
-                        />
-                      ))}
-                  </ol>
-                </div>
-              </div>
-              <div className="bookshelf">
-                <h2 className="bookshelf-title">Read</h2>
-                <div className="bookshelf-books">
-                  <ol className="books-grid">
-                    {books
-                      .filter((book) => book.shelf === "read")
-                      .map((book) => (
-                        <Book
-                          key={book.id}
-                          book={book}
-                          setBookShelf={setBookShelf}
-                        />
-                      ))}
-                  </ol>
-                </div>
+                <input
+                  type="text"
+                  onChange={(e) => {
+                    setNoResult(false);
+                    searchBooks(e.target.value);
+                  }}
+                  placeholder="Search by title or author"
+                />
               </div>
             </div>
+            <div className="search-books-results">
+              <ol className="books-grid">
+                {searchResult.map((book) => (
+                  <Book key={book.id} book={book} setBookShelf={setBookShelf} />
+                ))}
+                {noResult && <div>No Results where found </div>}
+              </ol>
+            </div>
           </div>
-          <div className="open-search">
-            <button onClick={() => setShowSearchPage(true)}>Add a book</button>
-          </div>
-        </div>
-      )}
+        </Route>
+      </Switch>
     </div>
   );
 }
